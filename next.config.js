1c57b7a12a76974b392d924d4365ad38f291afa5
mizunuma-t <mizunuma.takumi@gmail.com>
2025-05-29 12:29:17 +0900

    GitHubでのリンクで出力できるように編集　 next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  // 開発環境と本番環境の共通設定
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  
-  // 開発サーバーの設定
-  devIndicators: {
-    buildActivity: true,
+  trailingSlash: true,
+  assetPrefix: isProd ? '/JapanAI/' : '',
+  basePath: isProd ? '/JapanAI' : '',
+  images: {
+    unoptimized: true,
   },
-  
-  // すべてのホストからのアクセスを許可
-  experimental: {
-    // 最新の実験的機能設定
-  }
 };
-
-nextConfig.images = {
-  unoptimized: true,
-};
-nextConfig.trailingSlash = true;
-// 静的出力時にパス設定を修正
-nextConfig.assetPrefix = '/';
 
 module.exports = nextConfig;
